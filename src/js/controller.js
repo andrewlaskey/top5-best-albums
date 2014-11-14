57f87b4ece557520c9991456faa9597b5fb47386
andrewlaskey <andrew.laskey@gmail.com>
2014-11-14 10:35:07 -0800

    added some basic js

---
+++ src/js/controller.js
@@ -0,0 +1,24 @@
+app.controller('AppControl', function($scope, $firebase) {
+  var ref = new Firebase("https://top5.firebaseIO.com/");
+  $scope.albums = [];
+  angularFire(ref, $scope, "albums");
+  $scope.addAlbum = function(e) {
+
+    var index = contains($scope.albums, $scope.albumName);
+    if ( index >= 0) {
+      $scope.albums[index].score += (11 - $scope.userScore);
+      $scope.albums[index].listnames += ', ' + $scope.userName;
+    } else {
+      $scope.albums.push({
+        bandName: $scope.bandName,
+        albumName: $scope.albumName,
+        score: (11 - $scope.userScore),
+        listnames: $scope.userName
+      });  
+    }
+    
+    $scope.bandName = "";
+    $scope.albumName = "";
+    $scope.userScore = "";
+  };
+}
